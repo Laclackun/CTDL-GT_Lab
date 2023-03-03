2ae4ccc939220942797e83984973ee63dc7c839a
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-03 23:45:26 +0700

    Add files via upload

---
+++ C1/ToaDoXY.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct DiemToaDo
+{
+    float x, y;
+};
+
+void Nhap(DiemToaDo &td)
+{
+    printf("\nXin Hay Nhap Toa Do X = ");
+    scanf("%d", &td.x);
+    
+	printf("\nXin Hay Nhap Toa Do Y = ");
+    scanf("%d", &td.y);
+
+
+void XuatHonSo(DiemToaDo td)
+{
+    printf("(%d,%d)", td.x, td.y);
+}
+int main()
+{
+    DiemToaDo td;
+    Nhap(td);
+    Xuat(td);
+    getch();
+    return 0;
+}
