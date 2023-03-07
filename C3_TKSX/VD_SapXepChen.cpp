0c3c793e78843d44d7a358b54fc84d24c5410c23
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-07 15:20:34 +0700

    Add files via upload

---
+++ C3_TKSX/VD_SapXepChen.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <conio.h>
+
+void DoiCho(int &a, int &b)
+{
+	int x = a;
+	a = b;
+	b = x;
+}
+
+void SapXepChen(int a[], int n) 
+{ 
+    int i, key, j;
+   for (i = 1; i < n; i++)
+   {
+       key = a[i];
+       j = i-1;
+       while (j >= 0 && a[j] > key)
+       {
+           a[j+1] = a[j];
+           j = j-1;
+       }
+       a[j+1] = key;
+   }
+} 
+int main()
+{
+	int a[] = {9, 2, 15, 30, 5, 21, 16, 20, 3, 10};
+    int n = sizeof (a)/sizeof(a[0]);
+    SapXepChen(a,n);
+    printf("Mang Sau Khi Sap Xep La:\n"); 
+    for(int i=0; i<n; i++)
+        printf(" %d ",a[i]);
+    getch();
+	return 0;
+}
