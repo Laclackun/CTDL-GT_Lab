e6de0af3944c8fd84ffe54a68f521dec84b9e8ff
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-07 16:45:41 +0700

    Update VD_SapXepChen.cpp

--- C3_TKSX/VD_SapXepChen.cpp
+++ C3_TKSX/VD_SapXepChen.cpp
@@ -25,12 +25,12 @@ void SapXepChen(int a[], int n)
 } 
 int main()
 {
-	int a[] = {9, 2, 15, 30, 5, 21, 16, 20, 3, 10};
+    int a[] = {9, 2, 15, 30, 5, 21, 16, 20, 3, 10};
     int n = sizeof (a)/sizeof(a[0]);
     SapXepChen(a,n);
     printf("Mang Sau Khi Sap Xep La:\n"); 
     for(int i=0; i<n; i++)
-        printf(" %d ",a[i]);
+    printf(" %d ",a[i]);
     getch();
-	return 0;
+    return 0;
 }
