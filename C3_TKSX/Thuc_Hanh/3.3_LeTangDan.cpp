e5fd4af1c62939ae72a48e34890095eff401435d
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-14 21:08:56 +0700

    Add files via upload

---
+++ C3_TKSX/Thuc_Hanh/3.3_LeTangDan.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#define Max 50
+
+void NhapMang (int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("\na[%d] = ", i);
+		scanf("%d", &a[i]);
+		if (a[i] == 0)
+			break;
+	}
+}
+
+void XuatMang(int a[], int n)
+{
+    for(int i = 0; i <= n - 1; i++)
+        printf("%d \n" ,a[i]);
+}
+
+void DoiCho(int &a, int &b)
+{
+	int x = a;
+	a = b;
+	b = x;
+}
+
+void SapXepChonChoTT(int a[], int n) 
+{ 
+    int i,j,nn;
+	for(i = 0; i < n-1; i++)
+	{
+		nn = i;
+		if (a[i] % 2 == 1)
+			{	
+			for (j = i+1; j < n; j++)
+			if (a[j] % 2 == 1 && a[j] < a[nn])
+			nn = j;
+ 			DoiCho(a[nn], a[i]);
+			}
+    }
+} 
+int main()
+{
+    int a[Max], n;
+	do
+	{
+	printf("Xin Hay Nhap So Phan Tu Trong Mang = ");
+	scanf("%d", &n);
+	} while (n>Max && printf("So Phan Tu Qua Muc."));
+    NhapMang(a, n);
+    SapXepChonChoTT (a, n);
+    printf("\nMang Sau Khi Sap Xep La:\n");
+ 	XuatMang(a, n);
+}
