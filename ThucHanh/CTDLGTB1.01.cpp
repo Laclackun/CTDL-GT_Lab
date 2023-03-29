5f6370e268408af571e359e11988d32573e56f36
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-29 13:08:50 +0700

    Create CTDLGTB1.01.cpp

---
+++ ThucHanh/CTDLGTB1.01.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+#include <conio.h>
+#include <math.h>
+#define Max 50
+
+void NhapMang (int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("\na[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+}
+
+long TongSoChan (int a[], int n)
+{
+	if(n==0)
+		return 0;
+	if (a[n-1] % 2 == 0)
+		return (TongSoChan(a, n-1) + a[n-1]);	
+	else
+		return (TongSoChan(a, n-1));
+}
+  
+int main() 
+{ 
+ 	int n, T, a[Max];
+ 	do
+	{
+	printf("Xin Hay Nhap So Phan Tu Trong Mang = ");
+	scanf("%d", &n);
+	} while (n>Max && printf("So Phan Tu Qua Muc."));
+ 	NhapMang(a,n);
+ 	T = TongSoChan(a,n);
+ 	printf("\nT = %d", T);
+    getch();
+	return 0; 
+}
